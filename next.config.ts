191ab503879b15189de828b37567eff1b50950c8
Sunil Kunchoor <sunilkunchoor@gmail.com>
2026-07-19 22:40:54 +0100

    Reverting the design back.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/knowledge-hub',
+  basePath: '/articles',
   images: {
     unoptimized: true,
   },
